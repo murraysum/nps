d1739fb5cce6823640493cb6664016e784a0e2b8
Murray Summers <murray.sum@gmail.com>
2014-02-16 22:53:13 +0000

    Document the survey class.

--- lib/nps/survey.rb
+++ lib/nps/survey.rb
@@ -3,32 +3,47 @@ module Nps
 
     attr_reader :ballot
 
+    # Public: Initialize a new Net Promoter Score (NPS) survey
+    #
+    # Example:
+    #   survey = Nps::Survey.new
+    #   survey.vote!(10)
+    #   survey.score
+    #
     def initialize
       @ballot = Nps.configuration.persistance.new
     end
 
+    # Public: The Net Promoter Score (NPS) for the survey.
     def score
       calculator.score
     end
 
+    # Public: The percentage of promoter votes in the survey.
     def percentage_of_promoters
       calculator.percentage_of_promoters
     end
 
+    # Public: The percentage of passive votes in the survey.
     def percentage_of_passives
       calculator.percentage_of_passives
     end
 
+    # Public: The percentage of detractor votes in the survey.
     def percentage_of_detractors
       calculator.percentage_of_detractors
     end
 
+    # Public: Add a vote to the survey with the given value
+    #
+    # value - The value of the vote on the NPS scale (0..10)
     def vote!(value)
       ballot.vote!(value)
     end
 
     private
 
+    # Internal: The Net Promoter Score Calculator
     def calculator
       Nps::Calculator.new(ballot)
     end
