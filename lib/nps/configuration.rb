756860b429b53a130e3e53c7b9e797b106556b78
Murray Summers <murray.sum@gmail.com>
2014-02-17 22:15:12 +0000

    Add documentation to the configuration class.

--- lib/nps/configuration.rb
+++ lib/nps/configuration.rb
@@ -1,8 +1,11 @@
 module Nps
   class Configuration
 
+    # Public: Retrieve or assign a persistance class to store the NPS votes.
     attr_accessor :persistance
 
+    # Public: Initialize a new configuration for the library
+    # with the default behaviour.
     def initialize
       @persistance = Nps::Ballot
     end
